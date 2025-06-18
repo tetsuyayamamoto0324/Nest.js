cd52d7f735282da5f49a5ce26db0246b245ff761
tetsuyayamamoto0324 <manpuku769@gmail.com>
2025-06-19 00:32:56 +0900

    途中1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+import { ItemsService } from './items/items.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ItemsModule],
+  controllers: [],
+  providers: [ItemsService],
 })
 export class AppModule {}
---
+++ src/items/items.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './items.model';
+
+@Injectable()
+export class ItemsService {
+  private items: Item[] = [];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    const items = this.items.find((item) => item.id === id);
+    if (!items) {
+      throw new Error('商品が存在しません');
+    }
+    return items;
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+}
+
